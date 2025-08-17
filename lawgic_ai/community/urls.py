3317f2e8bee78f04f5aafc28fa0607d6545f4120
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-08-17 22:23:31 +0530

    add login

--- lawgic_ai/community/urls.py
+++ lawgic_ai/community/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from community import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("login", views.login_user, name="login_user"),
+    path("logout", views.logout_user, name="logoutuser")
 ]
--- lawgic_ai/community/views.py
+++ lawgic_ai/community/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
-from django.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
+    
+    if request.user.is_authenticated:
+        return render(request, 'welcome.html')
+    
+    else:
+        return redirect('login_user')
+
+
+def login_user(request):
+    
+    if request.method == "POST":
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username, password=password)
+
+        if user is not None:
+
+            login(request, user)
+
+            return redirect('home')
+        
+        else:
+
+            return render(request, 'login.html')
+    
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+
+    logout(request)
+
     return render(request, 'home.html')
+
 
 
