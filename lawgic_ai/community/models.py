9d44805afc89f9e8ad8a3be5776e13c57b383071
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-08-21 22:28:08 +0530

    add post

--- lawgic_ai/community/models.py
+++ lawgic_ai/community/models.py
@@ -10,3 +10,14 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title) + ' | ' + str(self.author) + ' | ' + str(self.date)
+    
+
