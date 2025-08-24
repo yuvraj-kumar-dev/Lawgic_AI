825e0dc08137fd479eeea6375b3dae858b0deedc
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-08-24 10:57:58 +0530

    add custom forms and fixed security issues

--- lawgic_ai/community/forms.py
+++ lawgic_ai/community/forms.py
@@ -1,5 +1,14 @@
-# from django import forms
-# from .models import Post
+from django import forms
+from .models import Post
 
-# class AddPost(forms):
-#     class Meta:
+class AddPost(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("title", "body")
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Add Title'}),
+            # 'author':forms.Select(attrs={'class':'form-control', 'placeholder': 'Add Title'}),
+            'body':forms.Textarea(attrs={'class':'form-control', 'placeholder': 'Add Title'}),
+
+        }
--- lawgic_ai/community/views.py
+++ lawgic_ai/community/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import AddPost
 
 # Create your views here.
 
@@ -59,6 +63,7 @@ def signup_user(request):
 
     return render(request, 'signup.html')
 
+@login_required
 def profile(request):
 
     username = request.user.username
@@ -67,18 +72,24 @@ def profile(request):
 
     return render(request, 'profile.html', context)
 
-class community(ListView):
+class community(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'community.html'
 
-class post(DetailView):
+class post(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post.html'
 
-class addpost(CreateView):
+class addpost(LoginRequiredMixin, CreateView):
     model = Post
+    form_class = AddPost
     template_name = 'addpost.html'
-    fields = '__all__'
+    # fields = '__all__'
+    success_url = reverse_lazy('community')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 
