7627191c69742dac90c82c2e09de18f7ebe8223a
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-08-28 21:41:34 +0530

    add prompt template

--- lawgic_ai/chatbot/views.py
+++ lawgic_ai/chatbot/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-# from langchain.community import prompttemplate
+from django.contrib.auth.decorators import login_required
+from langchain_core.prompts import PromptTemplate
 
 # Create your views here.
 
+PROMPT = PromptTemplate(
+    """You are a **Professional Legal Adviser** whose task is to give the Best and Verified Legal Advice 
+    to the **User** and If you are not sure about any advice then it is best to tell the user to contact
+    any legal advisor rather than telling any unverified suggestions"""
+)
+
+@login_required
 def chatbot(request):
     return render(request, 'chatbot.html')
