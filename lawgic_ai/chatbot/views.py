eb0419fbc1bfeac83fcacf16767a425b0cf8b528
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-09-07 18:23:26 +0530

    add elements to fix ui

--- lawgic_ai/chatbot/views.py
+++ lawgic_ai/chatbot/views.py
@@ -4,11 +4,11 @@ from langchain_core.prompts import PromptTemplate
 
 # Create your views here.
 
-PROMPT = PromptTemplate(
-    """You are a **Professional Legal Adviser** whose task is to give the Best and Verified Legal Advice 
-    to the **User** and If you are not sure about any advice then it is best to tell the user to contact
-    any legal advisor rather than telling any unverified suggestions"""
-)
+# PROMPT = PromptTemplate(
+#     """You are a **Professional Legal Adviser** whose task is to give the Best and Verified Legal Advice 
+#     to the **User** and If you are not sure about any advice then it is best to tell the user to contact
+#     any legal advisor rather than telling any unverified suggestions"""
+# )
 
 @login_required
 def chatbot(request):
